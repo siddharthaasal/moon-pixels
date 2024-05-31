4bd1be3f56cf0b23303871a0a8ff94e46833944b
siddharthaasal <siddharthaasal@gmail.com>
2024-05-31 23:31:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+// Function to display the selected image
+function displayImage(event, src) {
+  event.preventDefault(); // Prevent the default link behavior
+  var imageElement = document.getElementById("displayedImage");
+  imageElement.src = src;
+  imageElement.style.display = "block";
+}
